5821b2320e6a32c970909cb06fea40ef62b68b56
MukeshPurohit <purohit.mukesh@gmail.com>
2017-08-27 10:33:36 +0000

    initial commit

---
+++ V1_2_1__Add_More_people.sql
@@ -0,0 +1,6 @@
+insert into PERSON (ID, NAME) values (7, 'Zee');
+insert into PERSON (ID, NAME) values (8, 'Alex');
+insert into PERSON (ID, NAME) values (9, 'Davis');
+insert into PERSON (ID, NAME) values (10, 'Ram');
+insert into PERSON (ID, NAME) values (11, 'Mohan');
+insert into PERSON (ID, NAME) values (12, 'Babita');
---
+++ V1_2__Add_people.sql
@@ -0,0 +1,6 @@
+insert into PERSON (ID, NAME) values (1, 'Mukesh Purohit');
+insert into PERSON (ID, NAME) values (2, 'Zunaid');
+insert into PERSON (ID, NAME) values (3, 'Rohit');
+insert into PERSON (ID, NAME) values (4, 'Raj');
+insert into PERSON (ID, NAME) values (5, 'Boban');
+insert into PERSON (ID, NAME) values (6, 'Umesh');

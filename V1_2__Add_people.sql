4b19729babc263e89987b22ddfb208b5a2640a9b
MukeshPurohit <purohit.mukesh@gmail.com>
2017-08-29 18:32:56 +0530

    Delete V1_2__Add_people.sql

--- V1_2__Add_people.sql
+++
@@ -1,6 +0,0 @@
-insert into PERSON (ID, NAME) values (1, 'Mukesh Purohit');
-insert into PERSON (ID, NAME) values (2, 'Zunaid');
-insert into PERSON (ID, NAME) values (3, 'Rohit');
-insert into PERSON (ID, NAME) values (4, 'Raj');
-insert into PERSON (ID, NAME) values (5, 'Boban');
-insert into PERSON (ID, NAME) values (6, 'Umesh');
